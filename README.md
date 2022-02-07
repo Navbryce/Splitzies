4ccda6f3a4b9ef1b56ecbb71031ce4a0303f7881
Bryce Plunkett <bryce@lawn-128-61-115-93.lawn.gatech.edu>
2022-02-06 22:44:51 -0500

    Netlify config

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # splitzies
+TBD
 
 ## CLI Commands
 *   `npm install`: Installs dependencies
@@ -14,6 +15,3 @@
 *   `npm run test`: Run Jest and Enzyme with
     [`enzyme-adapter-preact-pure`](https://github.com/preactjs/enzyme-adapter-preact-pure) for
     your tests
-
-
-For detailed explanation on how things work, checkout the [CLI Readme](https://github.com/developit/preact-cli/blob/master/README.md).
--- preact.config.js
+++ preact.config.js
@@ -1,15 +1,15 @@
 import {DefinePlugin} from 'webpack';
 
-const { parsed } = require('dotenv-safe').config();
+const { required } = require('dotenv-safe').config();
 
 export default (config, env, helpers) => {
     config.resolve.alias.src = env.src;
     // dotenv injection
     config.plugins.push(new DefinePlugin(
-        Object.keys(parsed).reduce(
+        Object.keys(required).reduce(
             (env, key) => ({
                 ...env,
-                [`process.env.${key}`]: JSON.stringify(parsed[key]),
+                [`process.env.${key}`]: JSON.stringify(required[key]),
             }),
             {}
         ))
